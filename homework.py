3181886ac36d8044611dd3a4222fab83c7c34d66
Darima98 <165848832+Darima98@users.noreply.github.com>
2024-04-14 10:56:38 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,17 @@
+my_list = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for i in my_list:
+    if i == 'BMW':
+        print('Я езжу на автомабиле марки ' + i)
+    if i == 'MB':
+        print('Я езжу на автомабиле марки ' + i)
+    if i == 'LADA':
+        print('Я езжу на автомабиле марки ' + i)
+    if i == 'KIA':
+        print('Я езжу на автомабиле марки ' + i)
+    if i == 'HONDA':
+        print('Я езжу на автомабиле марки ' + i)
+cars_count = 0
+cars_count += 10
+for i in range(len(my_list)):
+    cars_count += 10
+print(cars_count)
